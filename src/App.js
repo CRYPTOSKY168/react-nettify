57a783f4d7651e50e10fc1c7fa0e37c96d2abeff
sarawuth thongdee <phiopan@MacBook-Pro-khxng-sarawuth.local>
2024-11-07 09:00:02 +0700

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit11 <code>src/App.js</code> and save to reload.
+          สวัดดี <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
